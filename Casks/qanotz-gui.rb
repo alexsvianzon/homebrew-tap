dfce9cc4193f284bcdba2cbeafe7ec24d9e42bb6
alexsvianzon <alexsvianzon@gmail.com>
2026-05-02 19:44:41 -0500

    take 2: add qanotz

---
+++ Casks/qanotz-gui.rb
@@ -0,0 +1,11 @@
+cask "qanotz-gui" do
+  version "1.0.0"
+  sha256 "249fd9a3919b64f62b9e217326ce2e797f467530e1f7376defede7af679d5925"
+
+  url "https://github.com/alexsvianzon/qanotz/releases/download/1.0.1/QANotz-macos-1.0.1.zip"
+  name "QANotz"
+  desc "QANotz is a small but powerful personal knowledge app built with Python and tkinter."
+  homepage "https://alexsvianzon.github.io/qanotz/"
+
+  app "QANotz.app"
+end

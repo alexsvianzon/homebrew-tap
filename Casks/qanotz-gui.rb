feb873b72f2c5d1a04b9ce70a71e7c25b70cd401
alexsvianzon <alexsvianzon@gmail.com>
2026-05-02 19:53:45 -0500

    fix for style and audit

--- Casks/qanotz-gui.rb
+++ Casks/qanotz-gui.rb
@@ -1,10 +1,10 @@
 cask "qanotz-gui" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 "249fd9a3919b64f62b9e217326ce2e797f467530e1f7376defede7af679d5925"
 
-  url "https://github.com/alexsvianzon/qanotz/releases/download/1.0.1/QANotz-macos-1.0.1.zip"
+  url "https://github.com/alexsvianzon/qanotz/releases/download/#{version}/QANotz-macos-#{version}.zip"
   name "QANotz"
-  desc "QANotz is a small but powerful personal knowledge app built with Python and tkinter."
+  desc "Simple note taking app"
   homepage "https://alexsvianzon.github.io/qanotz/"
 
   app "QANotz.app"
